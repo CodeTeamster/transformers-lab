7200f66134bd65db61a6697c7d9bc442bc0d339d
rychoy <riechan.ye@outlook.com>
2025-12-16 07:52:06 +0000

    Update transformers to 7f52a2a4ea8ab49b7f069df7fac58a5b280d4919.

--- lab/main.py
+++ lab/main.py
@@ -1,9 +1,9 @@
-from src.transformers_qwen2_5_vl_eval import run_eval as qwen_eval, defaultargs as qwen_eval_defaultargs
-from src.transformers_qwen2_5_vl_clip_metric import run_eval as qwen_clip_eval, defaultargs as qwen_clip_eval_defaultargs
-from src.transformers_vit_eval import run_eval as tf_eval, defaultargs as tf_eval_defaultargs
-from src.timm_vit_train import run_train as timm_train, defaultargs as timm_train_defaultargs
-from src.timm_vit_eval import run_eval as timm_eval, defaultargs as timm_eval_defaultargs
-from utils import plot_multi_performance, plot_performance, plot_from_csv
+# from src.transformers_qwen2_5_vl_eval import run_eval as qwen_eval, defaultargs as qwen_eval_defaultargs
+# from src.transformers_qwen2_5_vl_clip_metric import run_eval as qwen_clip_eval, defaultargs as qwen_clip_eval_defaultargs
+# from src.transformers_vit_eval import run_eval as tf_eval, defaultargs as tf_eval_defaultargs
+# from src.timm_vit_train import run_train as timm_train, defaultargs as timm_train_defaultargs
+# from src.timm_vit_eval import run_eval as timm_eval, defaultargs as timm_eval_defaultargs
+# from utils import plot_multi_performance, plot_performance, plot_from_csv
 from transformers.models.vit.convert_vit_timm_to_pytorch import convert_vit_checkpoint
 from transformers.models.deit.convert_deit_timm_to_pytorch import convert_deit_checkpoint
 
@@ -57,10 +57,15 @@ tome_perf = [
 
 
 if __name__ == "__main__":
-    convert_deit_checkpoint(
-        'deit_base_patch16_224.fb_in1k',
-        '/home/jovyan/nas/yrc/model/timm/deit_base_patch16_224.fb_in1k/model.safetensors',
-        '/home/jovyan/nas/yrc/model/transformers/deit_base_patch16_224.fb_in1k/',
+    # convert_deit_checkpoint(
+    #     'deit_base_distilled_patch16_224',
+    #     '/home/jovyan/nas/yrc/model/timm/deit_base_distilled_patch16_224.fb_in1k/model.safetensors',
+    #     '/home/jovyan/nas/yrc/model/transformers/deit_base_distilled_patch16_224.fb_in1k/',
+    # )
+    convert_vit_checkpoint(
+        'deit3_base_patch16_224',
+        '/home/jovyan/nas/yrc/model/timm/deit3_base_patch16_224.fb_in1k/model.safetensors',
+        '/home/jovyan/nas/yrc/model/transformers/deit3_base_patch16_224.fb_in1k/',
     )
     # model = ViTGraftConfig()
     # model.save_pretrained("./config.json")
@@ -92,13 +97,13 @@ if __name__ == "__main__":
     #     plot_performance(perf_dir=f"./workdir/{model[i]}.perf",
     #                      save_path=f"./workdir/{model[i]}.perf/performance.png")
 
-    plot_multi_performance(
-        dirs_and_files_regex=discard_perf_vertical,
-        indice_type="discard",
-        save_path="./workdir/discard-sup-0-6-multi-perf.png",
-        # baseline_x=0.336,
-        # baseline_y=80.5,
-    )
+    # plot_multi_performance(
+    #     dirs_and_files_regex=discard_perf_vertical,
+    #     indice_type="discard",
+    #     save_path="./workdir/discard-sup-0-6-multi-perf.png",
+    #     baseline_x=0.336,
+    #     baseline_y=80.5,
+    # )
     # plot_multi_performance(
     #     dirs_and_files_regex=discard_perf_horizontal,
     #     indice_type="discard",
