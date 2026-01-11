ece8cce09645931ae7be29db3e332cbc1888c65c
rychoy <riechan.ye@outlook.com>
2026-01-11 06:35:54 +0000

    The newly added logic that the discard rate can be uniformly sampled.

--- lab/vit_rd_distill_train.sh
+++ lab/vit_rd_distill_train.sh
@@ -2,13 +2,13 @@
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 export TORCH_CPP_LOG_LEVEL=ERROR
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 NUM_DEVICES=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
-BASE_PATH="/starry-nas"
-TEACHER_MODEL_PATH='{"'"$BASE_PATH"'/yrc/model/transformers/vit_base_patch16_224.augreg2_in21k_ft_in1k/": "transformers"}'
-STUDENT_MODEL_PATH="$BASE_PATH/yrc/model/yeruichen/vit-rd-base-patch16-224.augreg2_in21k_ft_in1k.sup-layerwise1/"
+BASE_PATH="/home/jovyan/nas"
+TEACHER_MODEL_PATH='{"'"$BASE_PATH"'/yrc/model/yeruichen/vit-rd-base-patch16-224.sup-0/": "transformers"}'
+STUDENT_MODEL_PATH="$BASE_PATH/yrc/model/yeruichen/vit-rd-base-patch16-224.sup-12/"
 DATASET_PATH="$BASE_PATH/yrc/dataset/imagenet-1k/arrow/"
-CKPT_SAVE_PATH="./ckpts/vit-rd-base-patch16-224.augreg2_in21k_ft_in1k.sup-layerwise1-discard-0.4-layer-0"
+CKPT_SAVE_PATH="./ckpts/vit.sup-12-discard-ud0.3-layer-all"
 
 accelerate launch --num_processes=$NUM_DEVICES \
     src/vit_rd_distill_train.py \
@@ -17,16 +17,16 @@ accelerate launch --num_processes=$NUM_DEVICES \
     --dataset-path="${DATASET_PATH}" \
     --image-processor-path="" \
     --ckpt-save-path="${CKPT_SAVE_PATH}" \
-    --ckpt-history-amount=0 \
+    --ckpt-history-amount=5 \
     --log-level="warning" \
     --layer-num=12 \
-    --learning-rate=1e-5 \
-    --weight-decay=2e-2 \
+    --learning-rate=2e-5 \
+    --weight-decay=1e-2 \
     --warmup-ratio=0.06 \
     --num-epochs=50 \
     --earlystop-patience=0 \
-    --discard-rate=0.4 \
-    --discard-before-layers 0 \
+    --discard-rate=-0.3 \
+    --discard-before-layers 0 1 2 3 4 5 6 7 8 9 10 11 \
     --temperature=5.0 \
     --alpha-param=0.2 \
     --beta-param=0.8
--- lab/vit_train.sh
+++ lab/vit_train.sh
@@ -2,12 +2,12 @@
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 export TORCH_CPP_LOG_LEVEL=ERROR
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 NUM_DEVICES=$(echo $CUDA_VISIBLE_DEVICES | awk -F',' '{print NF}')
 BASE_PATH="/home/jovyan/nas"
-PRETRAINED_MODEL_PATH="$BASE_PATH/yrc/model/yeruichen/vit-rd-base-patch16-224.augreg2_in21k_ft_in1k.sup-0/"
+PRETRAINED_MODEL_PATH="$BASE_PATH/yrc/model/yeruichen/vit-rd-base-patch16-224.sup-0/"
 DATASET_PATH="$BASE_PATH/yrc/dataset/imagenet-1k/arrow/"
-CKPT_SAVE_PATH="./ckpts/evit-discard-0.3-layer369"
+CKPT_SAVE_PATH="./ckpts/evit.discard-0.3-layer369"
 
 accelerate launch --num_processes=$NUM_DEVICES \
     src/transformers_vit_train.py \
@@ -15,10 +15,10 @@ accelerate launch --num_processes=$NUM_DEVICES \
     --dataset-path="${DATASET_PATH}" \
     --image-processor-path="" \
     --ckpt-save-path="${CKPT_SAVE_PATH}" \
-    --ckpt-history-amount=6 \
+    --ckpt-history-amount=5 \
     --log-level="warning" \
-    --learning-rate=1e-5 \
-    --weight-decay=2e-2 \
+    --learning-rate=2e-5 \
+    --weight-decay=1e-2 \
     --warmup-ratio=0.06 \
     --num-epochs=50 \
     --earlystop-patience=0 \
